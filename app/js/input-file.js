b186f8463e2942bbd0b8243ed48510836e981320
govarun23rus <govarun23cash@gmail.com>
2015-06-11 16:24:51 +0400

    Revert "egrter"
    This reverts commit 1b6a075943c31c839440019a7898ee5eacc2899b.

--- app/js/input-file.js
+++
@@ -1,9 +0,0 @@
-(function($){
-
-/* Update the label text */
-$('.input-file__input').on('change', function() {
-    var $input = $(this);
-    $input.siblings('.input-file__text').text($input.val().replace(/([^\\]*\\)*/,'') )
-});
-
-}(jQuery));
